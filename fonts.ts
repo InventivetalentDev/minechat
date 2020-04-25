453996fbc599b7414226c4fcaad08440374c212b
inventivetalent <mail@inventivetalent.org>
2020-04-25 15:33:55 +0200

    move purebd; add modernhd & conquest

--- fonts.ts
+++ fonts.ts
@@ -23,11 +23,27 @@ export const FONTS = [
   },
 
   {
-    name: 'bdcraft:128',
+    name: 'bdcraft:pure128',
     data: 'https://cdn.jsdelivr.net/gh/InventivetalentDev/minecraft-assets@' + VERSION + '/assets/minecraft/font/default.json',
     imageBase: 'public/font_textures/bdcraft/',
     prefix: 'minecraft:',
     link: "https://bdcraft.net/downloads/purebdcraft-minecraft/",
     doNotCrop: true // disable automatic cropping here for now, since it cuts off too much when scaling up the default
+  },
+  {
+    name: 'mikeyto1o:modernhd',
+    data: 'https://cdn.jsdelivr.net/gh/InventivetalentDev/minecraft-assets@' + VERSION + '/assets/minecraft/font/default.json',
+    imageBase: 'public/font_textures/modern/',
+    prefix: 'minecraft:',
+    link: "https://www.planetminecraft.com/texture_pack/modern-hd-pack-64x-ctm-better-skies/",
+    doNotCrop: true
+  },
+  {
+    name: 'monsterfish_:conquest',
+    data: 'https://cdn.jsdelivr.net/gh/InventivetalentDev/minecraft-assets@' + VERSION + '/assets/minecraft/font/default.json',
+    imageBase: 'public/font_textures/conquest/',
+    prefix: 'minecraft:',
+    link: "https://www.planetminecraft.com/texture_pack/conquest-32x32/",
+    doNotCrop: true
   }
 ];
